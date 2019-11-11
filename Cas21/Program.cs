249a60fac34edd3460c02778cc8e8d3ebb4c361f
Skola Rajak QA <skolarajak.qa@gmail.com>
2019-11-11 20:10:44 +0100

    Add project files.

---
+++ Cas21/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cas21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            bool? isItTrue = false;
+
+            if (isItTrue != null)
+            {
+                if (isItTrue == true)
+                {
+                    Console.WriteLine("Value is TRUE.");
+                }
+                else
+                {
+                    Console.WriteLine("Value is FALSE.");
+                }
+            } else
+            {
+                Console.WriteLine("Value is not chosen.");
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
